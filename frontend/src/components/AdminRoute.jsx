85af27533783b7a0da3cf8d3303d25502d0aafd7
Parita Chauhan <paritac.we3vision@gmail.com>
2026-06-12 17:56:43 +0530

    update adminroute
    not much connected to UI. not much added

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -2,11 +2,27 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const AdminRoute = () => {
-  const { userInfo } = useSelector((state) => state.auth);
-  return userInfo && userInfo.isAdmin ? (
+  const { userInfo } = useSelector(
+    (state) => state.auth
+  );
+
+  if (!userInfo) {
+    return (
+      <Navigate
+        to='/login'
+        replace
+      />
+    );
+  }
+
+  return userInfo.isAdmin ? (
     <Outlet />
   ) : (
-    <Navigate to='/login' replace />
+    <Navigate
+      to='/unauthorized'
+      replace
+    />
   );
 };
+
 export default AdminRoute;
