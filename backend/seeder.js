bd4dc980368533123d61c8c8f4f2697ee6c36cc5
henilpatel2710 <henilp.we3vision@gmail.com>
2026-06-12 17:03:35 +0530

    12.10(seeder.js)
    seeder.js_alternative code_henil

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import colors from 'colors';
+
 import users from './data/users.js';
 import products from './data/products.js';
+
 import User from './models/userModel.js';
 import Product from './models/productModel.js';
 import Order from './models/orderModel.js';
+
 import connectDB from './config/db.js';
 
 dotenv.config();
@@ -14,22 +17,26 @@ connectDB();
 
 const importData = async () => {
   try {
-    await Order.deleteMany();
-    await Product.deleteMany();
-    await User.deleteMany();
+    await Promise.all([
+      Order.deleteMany(),
+      Product.deleteMany(),
+      User.deleteMany(),
+    ]);
 
     const createdUsers = await User.insertMany(users);
 
     const adminUser = createdUsers[0]._id;
 
-    const sampleProducts = products.map((product) => {
-      return { ...product, user: adminUser };
-    });
+    const sampleProducts = products.map((product) => ({
+      ...product,
+      user: adminUser,
+    }));
 
     await Product.insertMany(sampleProducts);
 
     console.log('Data Imported!'.green.inverse);
-    process.exit();
+
+    process.exit(0);
   } catch (error) {
     console.error(`${error}`.red.inverse);
     process.exit(1);
@@ -38,20 +45,19 @@ const importData = async () => {
 
 const destroyData = async () => {
   try {
-    await Order.deleteMany();
-    await Product.deleteMany();
-    await User.deleteMany();
+    await Promise.all([
+      Order.deleteMany(),
+      Product.deleteMany(),
+      User.deleteMany(),
+    ]);
 
     console.log('Data Destroyed!'.red.inverse);
-    process.exit();
+
+    process.exit(0);
   } catch (error) {
     console.error(`${error}`.red.inverse);
     process.exit(1);
   }
 };
 
-if (process.argv[2] === '-d') {
-  destroyData();
-} else {
-  importData();
-}
+process.argv[2] === '-d' ? destroyData() : importData();
