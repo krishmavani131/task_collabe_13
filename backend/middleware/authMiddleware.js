1578a27f092887e2ce7623b6011dad1d4c1b60b3
Chetan Jain <chetanj.we3vision@gmail.com>
2026-06-12 17:29:37 +0530

    12.9(authMiddleware.js)
    authMiddleware.js_alternativecode_Chetan

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,10 +4,7 @@ import User from '../models/userModel.js';
 
 // User must be authenticated
 const protect = asyncHandler(async (req, res, next) => {
-  let token;
-
-  // Read JWT from the 'jwt' cookie
-  token = req.cookies.jwt;
+  const token = req.cookies.jwt;
 
   if (token) {
     try {
