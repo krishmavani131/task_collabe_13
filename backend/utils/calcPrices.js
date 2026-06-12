208bfa2a2ef709e6335c6c19d827f09657dc7b7a
Chetan Jain <chetanj.we3vision@gmail.com>
2026-06-12 17:38:25 +0530

    12.9(calcPrices.js)
    calcPrices.js_alternativecode_Chetan

--- backend/utils/calcPrices.js
+++ backend/utils/calcPrices.js
@@ -1,6 +1,6 @@
-function addDecimals(num) {
+const addDecimals = (num) => {
   return (Math.round(num * 100) / 100).toFixed(2);
-}
+};
 
 // NOTE: the code below has been changed from the course code to fix an issue
 // with type coercion of strings to numbers.
@@ -8,23 +8,18 @@ function addDecimals(num) {
 // correct to call it passing a string as the argument.
 
 export function calcPrices(orderItems) {
-  // Calculate the items price in whole number (pennies) to avoid issues with
-  // floating point number calculations
   const itemsPrice = orderItems.reduce(
     (acc, item) => acc + (item.price * 100 * item.qty) / 100,
     0
   );
 
-  // Calculate the shipping price
   const shippingPrice = itemsPrice > 100 ? 0 : 10;
 
-  // Calculate the tax price
-  const taxPrice = 0.15 * itemsPrice;
+  const taxRate = 0.15;
+  const taxPrice = taxRate * itemsPrice;
 
-  // Calculate the total price
   const totalPrice = itemsPrice + shippingPrice + taxPrice;
 
-  // return prices as strings fixed to 2 decimal places
   return {
     itemsPrice: addDecimals(itemsPrice),
     shippingPrice: addDecimals(shippingPrice),
