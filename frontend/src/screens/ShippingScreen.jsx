984cb9af976d198bc93953144025eebedc76cb66
shivanirwe3vision-ctrl <shivanir.we3vision@gmail.com>
2026-06-13 11:08:35 +0530

    13.6(ShippingScreen.jsx)
    ShippingScreen.jsx_alternativecode_Shivani

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -2,80 +2,104 @@ import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+
 import FormContainer from '../components/FormContainer';
 import CheckoutSteps from '../components/CheckoutSteps';
 import { saveShippingAddress } from '../slices/cartSlice';
 
 const ShippingScreen = () => {
-  const cart = useSelector((state) => state.cart);
-  const { shippingAddress } = cart;
+  const { shippingAddress } = useSelector((state) => state.cart);
 
-  const [address, setAddress] = useState(shippingAddress.address || '');
-  const [city, setCity] = useState(shippingAddress.city || '');
-  const [postalCode, setPostalCode] = useState(
-    shippingAddress.postalCode || ''
-  );
-  const [country, setCountry] = useState(shippingAddress.country || '');
+  // single state object
+  const [formData, setFormData] = useState({
+    address: shippingAddress.address || '',
+    city: shippingAddress.city || '',
+    postalCode: shippingAddress.postalCode || '',
+    country: shippingAddress.country || '',
+  });
+
+  const { address, city, postalCode, country } = formData;
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const submitHandler = (e) => {
+  // common input handler
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  // submit handler
+  const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(saveShippingAddress({ address, city, postalCode, country }));
+
+    dispatch(saveShippingAddress(formData));
     navigate('/payment');
   };
 
   return (
     <FormContainer>
       <CheckoutSteps step1 step2 />
-      <h1>Shipping</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group className='my-2' controlId='address'>
+      <h1>Shipping Details</h1>
+
+      <Form onSubmit={handleSubmit}>
+        {/* Address */}
+        <Form.Group className="my-2" controlId="address">
           <Form.Label>Address</Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Enter address'
+            type="text"
+            name="address"
+            placeholder="Enter address"
             value={address}
             required
-            onChange={(e) => setAddress(e.target.value)}
-          ></Form.Control>
+            onChange={handleChange}
+          />
         </Form.Group>
 
-        <Form.Group className='my-2' controlId='city'>
+        {/* City */}
+        <Form.Group className="my-2" controlId="city">
           <Form.Label>City</Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Enter city'
+            type="text"
+            name="city"
+            placeholder="Enter city"
             value={city}
             required
-            onChange={(e) => setCity(e.target.value)}
-          ></Form.Control>
+            onChange={handleChange}
+          />
         </Form.Group>
 
-        <Form.Group className='my-2' controlId='postalCode'>
+        {/* Postal Code */}
+        <Form.Group className="my-2" controlId="postalCode">
           <Form.Label>Postal Code</Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Enter postal code'
+            type="text"
+            name="postalCode"
+            placeholder="Enter postal code"
             value={postalCode}
             required
-            onChange={(e) => setPostalCode(e.target.value)}
-          ></Form.Control>
+            onChange={handleChange}
+          />
         </Form.Group>
 
-        <Form.Group className='my-2' controlId='country'>
+        {/* Country */}
+        <Form.Group className="my-2" controlId="country">
           <Form.Label>Country</Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Enter country'
+            type="text"
+            name="country"
+            placeholder="Enter country"
             value={country}
             required
-            onChange={(e) => setCountry(e.target.value)}
-          ></Form.Control>
+            onChange={handleChange}
+          />
         </Form.Group>
 
-        <Button type='submit' variant='primary'>
+        <Button type="submit" variant="primary">
           Continue
         </Button>
       </Form>
