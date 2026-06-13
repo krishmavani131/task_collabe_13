43a31e372dfec34d0bb42dbd139356d383ebcecb
Parita Chauhan <paritac.we3vision@gmail.com>
2026-06-13 13:26:09 +0530

    Update AdminRoute.jsx

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -3,10 +3,23 @@ import { useSelector } from 'react-redux';
 
 function AdminRoute() {
   const { userInfo } = useSelector((state) => state.auth);
+
+<<<<<<< HEAD
+  const isAdmin = Boolean(userInfo && userInfo.isAdmin);
+
+  if (isAdmin) {
+    return <Outlet />;
+  }
+
+  return <Navigate to='/login' replace />;
+}
+=======
   return userInfo && userInfo.isAdmin ? (
     <Outlet />
   ) : (
     <Navigate to='/login' replace />
   );
 };
+>>>>>>> parita
+
 export default AdminRoute;
