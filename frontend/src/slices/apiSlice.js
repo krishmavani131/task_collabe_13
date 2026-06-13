2d89fc627d2f24aeef48cdd276d4a5aec3b541e2
Parita Chauhan <paritac.we3vision@gmail.com>
2026-06-13 10:50:05 +0530

    update apislice
    Automatic logout on expired token, shared caching system, cleaner API code

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,28 +1,50 @@
-import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
+import {
+  fetchBaseQuery,
+  createApi,
+} from '@reduxjs/toolkit/query/react';
+
 import { BASE_URL } from '../constants';
-
-import { logout } from './authSlice'; // Import the logout action
-
-// NOTE: code here has changed to handle when our JWT and Cookie expire.
-// We need to customize the baseQuery to be able to intercept any 401 responses
-// and log the user out
-// https://redux-toolkit.js.org/rtk-query/usage/customizing-queries#customizing-queries-with-basequery
+import { logout } from './authSlice';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
+  credentials: 'include',
 });
 
-async function baseQueryWithAuth(args, api, extra) {
-  const result = await baseQuery(args, api, extra);
-  // Dispatch the logout action on 401.
-  if (result.error && result.error.status === 401) {
+const baseQueryWithAuth = async (
+  args,
+  api,
+  extraOptions
+) => {
+  const result = await baseQuery(
+    args,
+    api,
+    extraOptions
+  );
+
+  if (
+    result?.error?.status === 401
+  ) {
+    console.warn(
+      'Session expired. Logging out user.'
+    );
+
     api.dispatch(logout());
   }
+
   return result;
-}
+};
 
 export const apiSlice = createApi({
-  baseQuery: baseQueryWithAuth, // Use the customized baseQuery
-  tagTypes: ['Product', 'Order', 'User'],
-  endpoints: (builder) => ({}),
+  reducerPath: 'api',
+
+  baseQuery: baseQueryWithAuth,
+
+  tagTypes: [
+    'Product',
+    'Order',
+    'User',
+  ],
+
+  endpoints: () => ({}),
 });
