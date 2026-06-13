d210c07f17409d372f21f3ed5dfda3b528f55a7a
janvisolanki021 <janvis.we3vision@gmail.com>
2026-06-13 10:34:10 +0530

    13.1(seeder.js)
    seeder.js(alternate code)
    by janvi

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import colors from 'colors';
+
 import users from './data/users.js';
 import products from './data/products.js';
+
 import User from './models/userModel.js';
 import Product from './models/productModel.js';
 import Order from './models/orderModel.js';
+
 import connectDB from './config/db.js';
 
 dotenv.config();
@@ -14,44 +17,43 @@ connectDB();
 
 const importData = async () => {
   try {
-    await Order.deleteMany();
-    await Product.deleteMany();
-    await User.deleteMany();
+    await Order.deleteMany({});
+    await Product.deleteMany({});
+    await User.deleteMany({});
 
-    const createdUsers = await User.insertMany(users);
+    const insertedUsers = await User.insertMany(users);
 
-    const adminUser = createdUsers[0]._id;
+    const adminId = insertedUsers[0]._id;
 
-    const sampleProducts = products.map((product) => {
-      return { ...product, user: adminUser };
-    });
+    const productData = products.map((item) => ({
+      ...item,
+      user: adminId,
+    }));
 
-    await Product.insertMany(sampleProducts);
+    await Product.insertMany(productData);
 
-    console.log('Data Imported!'.green.inverse);
-    process.exit();
-  } catch (error) {
-    console.error(`${error}`.red.inverse);
+    console.log('Data Imported Successfully!'.green.inverse);
+    process.exit(0);
+  } catch (err) {
+    console.error(`${err}`.red.inverse);
     process.exit(1);
   }
 };
 
 const destroyData = async () => {
   try {
-    await Order.deleteMany();
-    await Product.deleteMany();
-    await User.deleteMany();
+    await Order.deleteMany({});
+    await Product.deleteMany({});
+    await User.deleteMany({});
 
-    console.log('Data Destroyed!'.red.inverse);
-    process.exit();
-  } catch (error) {
-    console.error(`${error}`.red.inverse);
+    console.log('Data Removed Successfully!'.red.inverse);
+    process.exit(0);
+  } catch (err) {
+    console.error(`${err}`.red.inverse);
     process.exit(1);
   }
 };
 
-if (process.argv[2] === '-d') {
-  destroyData();
-} else {
-  importData();
-}
+const action = process.argv[2];
+
+action === '-d' ? destroyData() : importData();
