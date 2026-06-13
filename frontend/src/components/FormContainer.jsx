868160b3e9d9bb92179ea1fd8a910746c861dc82
tishapwe3vision <tishap.we3vision@gmail.com>
2026-06-13 10:40:19 +0530

    13.5(FormContainer)
    FormContainer_Alternative-code_Tishaa

--- frontend/src/components/FormContainer.jsx
+++ frontend/src/components/FormContainer.jsx
@@ -1,15 +1,20 @@
 import { Container, Row, Col } from 'react-bootstrap';
 
-const FormContainer = ({ children }) => {
+const COLUMN_SIZES = {
+  xs: 12,
+  md: 6,
+};
+
+function FormContainer({ children }) {
   return (
     <Container>
       <Row className='justify-content-md-center'>
-        <Col xs={12} md={6}>
+        <Col xs={COLUMN_SIZES.xs} md={COLUMN_SIZES.md}>
           {children}
         </Col>
       </Row>
     </Container>
   );
-};
+}
 
 export default FormContainer;
