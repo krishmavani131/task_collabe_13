479df1c4a860bdab9e951bd8b6494c08306a3e8f
dhruvisha17 <dhruvishav.we3vision@gmail.com>
2026-06-13 10:46:18 +0530

    13.3(orderApiSlice.js)
    orderApiSlice.js_alternative code
    by Dhruvisha

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -1,54 +1,62 @@
-import { apiSlice } from './apiSlice';
-import { ORDERS_URL, PAYPAL_URL } from '../constants';
+import { apiSlice } from "./apiSlice";
+import { ORDERS_URL, PAYPAL_URL } from "../constants";
 
 export const orderApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
+
     createOrder: builder.mutation({
-      query: (order) => ({
+      query: (orderData) => ({
         url: ORDERS_URL,
-        method: 'POST',
-        body: order,
+        method: "POST",
+        body: orderData,
       }),
     }),
+
     getOrderDetails: builder.query({
-      query: (id) => ({
-        url: `${ORDERS_URL}/${id}`,
+      query: (orderId) => ({
+        url: `${ORDERS_URL}/${orderId}`,
       }),
       keepUnusedDataFor: 5,
+      providesTags: ["Order"],
     }),
+
     payOrder: builder.mutation({
       query: ({ orderId, details }) => ({
         url: `${ORDERS_URL}/${orderId}/pay`,
-        method: 'PUT',
+        method: "PUT",
         body: details,
       }),
+      invalidatesTags: ["Order"],
     }),
+
     getPaypalClientId: builder.query({
-      query: () => ({
-        url: PAYPAL_URL,
-      }),
+      query: () => PAYPAL_URL,
       keepUnusedDataFor: 5,
     }),
+
     getMyOrders: builder.query({
-      query: () => ({
-        url: `${ORDERS_URL}/mine`,
-      }),
+      query: () => `${ORDERS_URL}/mine`,
       keepUnusedDataFor: 5,
+      providesTags: ["Order"],
     }),
+
     getOrders: builder.query({
-      query: () => ({
-        url: ORDERS_URL,
-      }),
+      query: () => ORDERS_URL,
       keepUnusedDataFor: 5,
+      providesTags: ["Order"],
     }),
+
     deliverOrder: builder.mutation({
       query: (orderId) => ({
         url: `${ORDERS_URL}/${orderId}/deliver`,
-        method: 'PUT',
+        method: "PUT",
       }),
+      invalidatesTags: ["Order"],
     }),
+
   }),
 });
+
 
 export const {
   useCreateOrderMutation,
