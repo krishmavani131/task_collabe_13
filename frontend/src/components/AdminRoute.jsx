7297bc46a7da0620bfebc30d1e93144de6f229bc
tishapwe3vision <tishap.we3vision@gmail.com>
2026-06-13 10:12:36 +0530

    13.5(AdminRoute.jsx)
    AdminRoute.jsx_Alternative-code_Tishaa

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -1,12 +1,16 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-const AdminRoute = () => {
+function AdminRoute() {
   const { userInfo } = useSelector((state) => state.auth);
-  return userInfo && userInfo.isAdmin ? (
-    <Outlet />
-  ) : (
-    <Navigate to='/login' replace />
-  );
-};
+
+  const isAdmin = Boolean(userInfo && userInfo.isAdmin);
+
+  if (isAdmin) {
+    return <Outlet />;
+  }
+
+  return <Navigate to='/login' replace />;
+}
+
 export default AdminRoute;
