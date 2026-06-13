58a5640452bb5677ea5763b74d7937ed2794db99
dhruvisha17 <dhruvishav.we3vision@gmail.com>
2026-06-13 10:03:48 +0530

    13.3(apiSlice.js)
    apiSlice.js_alternative code
    by Dhruvisha

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,28 +1,25 @@
-import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
-import { BASE_URL } from '../constants';
-
-import { logout } from './authSlice'; // Import the logout action
-
-// NOTE: code here has changed to handle when our JWT and Cookie expire.
-// We need to customize the baseQuery to be able to intercept any 401 responses
-// and log the user out
-// https://redux-toolkit.js.org/rtk-query/usage/customizing-queries#customizing-queries-with-basequery
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../constants";
+import { logout } from "./authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
+  credentials: "include",
 });
 
-async function baseQueryWithAuth(args, api, extra) {
-  const result = await baseQuery(args, api, extra);
-  // Dispatch the logout action on 401.
-  if (result.error && result.error.status === 401) {
+const baseQueryHandler = async (request, api, extraOptions) => {
+  const response = await baseQuery(request, api, extraOptions);
+
+  if (response?.error?.status === 401) {
     api.dispatch(logout());
   }
-  return result;
-}
+
+  return response;
+};
 
 export const apiSlice = createApi({
-  baseQuery: baseQueryWithAuth, // Use the customized baseQuery
-  tagTypes: ['Product', 'Order', 'User'],
-  endpoints: (builder) => ({}),
+  reducerPath: "api",
+  baseQuery: baseQueryHandler,
+  tagTypes: ["Product", "Order", "User"],
+  endpoints: () => ({}),
 });
