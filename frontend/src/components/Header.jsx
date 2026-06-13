803e05a777fb129babb87ea86a710c5c7bd3a8fe
PruthaThakor <pruthat.we3vision@gmail.com>
2026-06-13 10:12:36 +0530

    13.5(Header.jsx)
    Header.jsx_alternate code_Prutha

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,25 +4,52 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate, Link } from 'react-router-dom';
 import { useLogoutMutation } from '../slices/usersApiSlice';
 import { logout } from '../slices/authSlice';
+import { resetCart } from '../slices/cartSlice';
 import SearchBox from './SearchBox';
 import logo from '../assets/logo.png';
-import { resetCart } from '../slices/cartSlice';
+
+const getCartItemCount = (cartItems) =>
+  cartItems.reduce((total, item) => total + item.qty, 0);
+
+const CartLink = ({ itemCount }) => (
+  <Nav.Link as={Link} to='/cart'>
+    <FaShoppingCart /> Cart
+    {itemCount > 0 && (
+      <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+        {itemCount}
+      </Badge>
+    )}
+  </Nav.Link>
+);
+
+const AdminMenu = () => (
+  <NavDropdown title='Admin' id='adminmenu'>
+    <NavDropdown.Item as={Link} to='/admin/productlist'>
+      Products
+    </NavDropdown.Item>
+    <NavDropdown.Item as={Link} to='/admin/orderlist'>
+      Orders
+    </NavDropdown.Item>
+    <NavDropdown.Item as={Link} to='/admin/userlist'>
+      Users
+    </NavDropdown.Item>
+  </NavDropdown>
+);
 
 const Header = () => {
-  const { cartItems } = useSelector((state) => state.cart);
-  const { userInfo } = useSelector((state) => state.auth);
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const { cartItems } = useSelector((state) => state.cart);
+  const { userInfo } = useSelector((state) => state.auth);
+
   const [logoutApiCall] = useLogoutMutation();
 
-  const logoutHandler = async () => {
+  const handleLogout = async () => {
     try {
       await logoutApiCall().unwrap();
       dispatch(logout());
-      // NOTE: here we need to reset cart state for when a user logs out so the next
-      // user doesn't inherit the previous users cart and shipping
+      // Reset the cart on logout so the next user doesn't inherit it
       dispatch(resetCart());
       navigate('/login');
     } catch (err) {
@@ -38,49 +65,31 @@ const Header = () => {
             <img src={logo} alt='ProShop' />
             ProShop
           </Navbar.Brand>
+
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
+
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
               <SearchBox />
-              <Nav.Link as={Link} to='/cart'>
-                <FaShoppingCart /> Cart
-                {cartItems.length > 0 && (
-                  <Badge pill bg='success' style={{ marginLeft: '5px' }}>
-                    {cartItems.reduce((a, c) => a + c.qty, 0)}
-                  </Badge>
-                )}
-              </Nav.Link>
+
+              <CartLink itemCount={getCartItemCount(cartItems)} />
+
               {userInfo ? (
-                <>
-                  <NavDropdown title={userInfo.name} id='username'>
-                    <NavDropdown.Item as={Link} to='/profile'>
-                      Profile
-                    </NavDropdown.Item>
-                    <NavDropdown.Item onClick={logoutHandler}>
-                      Logout
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </>
+                <NavDropdown title={userInfo.name} id='username'>
+                  <NavDropdown.Item as={Link} to='/profile'>
+                    Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item onClick={handleLogout}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
               ) : (
                 <Nav.Link as={Link} to='/login'>
                   <FaUser /> Sign In
                 </Nav.Link>
               )}
 
-              {/* Admin Links */}
-              {userInfo && userInfo.isAdmin && (
-                <NavDropdown title='Admin' id='adminmenu'>
-                  <NavDropdown.Item as={Link} to='/admin/productlist'>
-                    Products
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to='/admin/orderlist'>
-                    Orders
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to='/admin/userlist'>
-                    Users
-                  </NavDropdown.Item>
-                </NavDropdown>
-              )}
+              {userInfo?.isAdmin && <AdminMenu />}
             </Nav>
           </Navbar.Collapse>
         </Container>
